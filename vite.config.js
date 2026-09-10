e479c38af55c038034400536521df73161024f83
mohamedashif3101-coder <mohamedashif3101@gmail.com>
2026-09-10 20:54:42 +0530

    Fix import and plugin configuration in vite.config.js
    Fixed import statements and corrected plugin configuration.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react()],
+})
 })
